d6fb65c83bf834d3aace9f95629ca26489e0ae68
basnt ebrahim <161939675+BasntEbrahim7@users.noreply.github.com>
2024-09-10 05:15:58 +0300

    Update levels containers.dart

--- lib/Components/levels containers.dart
+++ lib/Components/levels containers.dart
@@ -4,29 +4,18 @@ import 'package:flutter_polygon/flutter_polygon.dart';
 class LevelsContainers extends StatelessWidget {
   final int levelNum;
   final Color containerColor;
-  final IconData starIconData; // Accept IconData
-  final Color starIconColor; // Accept color for the icon
 
   const LevelsContainers({
     super.key,
     required this.levelNum,
     required this.containerColor,
-    required this.starIconData,
-    this.starIconColor = Colors.black, // Default color
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Icon(starIconData, color: starIconColor,size: 40,), // Use IconData and color
-            Icon(starIconData, color: starIconColor,size: 40,),
-            Icon(starIconData, color: starIconColor,size: 40,),
-          ],
-        ),
+
         Container(
           width: 210,
           height: 220,
