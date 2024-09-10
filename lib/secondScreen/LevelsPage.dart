b5652900fa047b18d09ef1b609223c1f4ce97891
basnt ebrahim <161939675+BasntEbrahim7@users.noreply.github.com>
2024-09-10 05:17:43 +0300

    Update LevelsPage.dart

--- lib/secondScreen/LevelsPage.dart
+++ lib/secondScreen/LevelsPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:projects/TestPage/TastPage.dart';
-
 import '../Components/levels containers.dart';
 
 class LevelsPage extends StatelessWidget {
@@ -33,24 +32,41 @@ class LevelsPage extends StatelessWidget {
                 children: [
 
                   InkWell(
-                    child: LevelsContainers(
-                      levelNum: 01,
-                      containerColor: Color(0xff6c4bfc),
-                      starIconData: Icons.star_rate_sharp, // Use IconData
-                      starIconColor: Colors.yellowAccent,
-                      // Icon color
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.star_rate_sharp,color: Colors.yellowAccent,),
+                            Icon(Icons.star_rate_sharp,color: Colors.yellowAccent,),
+                            Icon(Icons.star_rate_sharp,color: Colors.yellowAccent,),
+                          ],
+                        ),
+                        LevelsContainers(
+                          levelNum: 01,
+                          containerColor: Color(0xff6c4bfc),
+                          
+                        ),
+                      ],
                     ),onTap: (){
                       Navigator.push(context,
                       MaterialPageRoute(builder: (context)=>TestPage(levelNumm: "level 1")));
-
                   },
                   ),
+                  
                   InkWell(
-                    child: LevelsContainers(
-                      levelNum: 02,
-                      containerColor: Color(0xff179afc),
-                      starIconData: Icons.star_rate_sharp, // Use IconData
-                      starIconColor: Colors.yellowAccent, // Add star color
+                    child: Column(
+                      children: [Row(children: [
+                        Icon(Icons.star_rate_sharp,color: Colors.yellowAccent,),
+                        Icon(Icons.star_rate_sharp,color: Colors.yellowAccent,),
+                        Icon(Icons.star_rate_sharp,color: Colors.yellowAccent,),
+
+                      ],),
+                        LevelsContainers(
+                          levelNum: 02,
+                          containerColor: Color(0xff179afc),
+
+                        ),
+                      ],
                     ),onTap: (){
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context)=>TestPage(levelNumm: "level 2")));
@@ -66,11 +82,20 @@ class LevelsPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   InkWell(
-                    child: LevelsContainers(
-                      levelNum: 03,
-                      containerColor: Color(0xfff96b2f),
-                      starIconData: Icons.star_rate_sharp, // Use IconData
-                      starIconColor: Colors.yellowAccent, // Add star color
+                    child: Column(
+                      children: [Row(
+                        children: [
+                          Icon(Icons.star_rate_sharp,color: Colors.yellowAccent,),
+                          Icon(Icons.star_rate_sharp,color: Colors.brown,),
+                          Icon(Icons.star_rate_sharp,color: Colors.brown,),
+                        ],
+                      ),
+                        LevelsContainers(
+                          levelNum: 03,
+                          containerColor: Color(0xfff96b2f),
+
+                        ),
+                      ],
                     ),onTap: (){
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context)=>TestPage(levelNumm: "level 3")));
@@ -79,11 +104,20 @@ class LevelsPage extends StatelessWidget {
                   },
                   ),
                   InkWell(
-                    child: LevelsContainers(
-                      levelNum: 04,
-                      containerColor: Color(0xff27caaf),
-                      starIconData: Icons.star_rate_sharp, // Use IconData
-                      starIconColor: Colors.yellowAccent, // Add star color
+                    child: Column(
+                      children: [Row(
+                        children: [
+                          Icon(Icons.star_rate_sharp,color: Colors.yellowAccent,),
+                          Icon(Icons.star_rate_sharp,color: Colors.yellowAccent,),
+                          Icon(Icons.star_rate_sharp,color: Colors.brown,),
+                        ],
+                      ),
+                        LevelsContainers(
+                          levelNum: 04,
+                          containerColor: Color(0xff27caaf),
+
+                        ),
+                      ],
                     ),onTap: (){
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context)=>TestPage(levelNumm: "level 4")));
@@ -99,11 +133,18 @@ class LevelsPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   InkWell(
-                    child: LevelsContainers(
-                      levelNum: 05,
-                      containerColor: Color(0xfff3319e),
-                      starIconData: Icons.star_rate_sharp, // Use IconData
-                      starIconColor: Colors.yellowAccent, // Add star color
+                    child: Column(
+                      children: [Row(children: [
+                        Icon(Icons.star_border,),
+                        Icon(Icons.star_border,),
+                        Icon(Icons.star_border,),
+                      ],),
+                        LevelsContainers(
+                          levelNum: 05,
+                          containerColor: Color(0xfff3319e),
+
+                        ),
+                      ],
                     ),onTap: (){
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context)=>TestPage(levelNumm: "level 5")));
@@ -113,11 +154,18 @@ class LevelsPage extends StatelessWidget {
                   },
                   ),
                   InkWell(
-                    child: LevelsContainers(
-                      levelNum: 06,
-                      containerColor: Color(0xff473880),
-                      starIconData: Icons.star_rate_sharp, // Use IconData
-                      starIconColor: Colors.yellowAccent, // Add star color
+                    child: Column(
+                      children: [Row(children: [
+                        Icon(Icons.star_border),
+                        Icon(Icons.star_border),
+                        Icon(Icons.star_border),
+                      ],),
+                        LevelsContainers(
+                          levelNum: 06,
+                          containerColor: Color(0xff473880),
+
+                        ),
+                      ],
                     ),onTap: (){
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context)=>TestPage(levelNumm: "level 6")));
@@ -133,11 +181,20 @@ class LevelsPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   InkWell(
-                    child: LevelsContainers(
-                      levelNum: 07,
-                      containerColor: Color(0xff23487b),
-                      starIconData: Icons.star_rate_sharp, // Use IconData
-                      starIconColor: Colors.yellowAccent, // Add star color
+                    child: Column(
+                      children: [Row(children: [
+                        Icon(Icons.star_border),
+                        Icon(Icons.star_border),
+                        Icon(Icons.star_border),
+
+
+                      ],),
+                        LevelsContainers(
+                          levelNum: 07,
+                          containerColor: Color(0xff23487b),
+
+                        ),
+                      ],
                     ),onTap: (){
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context)=>TestPage(levelNumm: "level 7")));
@@ -146,11 +203,19 @@ class LevelsPage extends StatelessWidget {
                   },
                   ),
                   InkWell(
-                    child: LevelsContainers(
-                      levelNum: 08,
-                      containerColor: Color(0xff634051),
-                      starIconData: Icons.star_rate_sharp, // Use IconData
-                      starIconColor: Colors.yellowAccent, // Add star color
+                    child: Column(
+                      children: [Row(children: [
+                        Icon(Icons.star_border),
+                        Icon(Icons.star_border),
+                        Icon(Icons.star_border),
+
+                      ],),
+                        LevelsContainers(
+                          levelNum: 08,
+                          containerColor: Color(0xff634051),
+
+                        ),
+                      ],
                     ),onTap: (){
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context)=>TestPage(levelNumm: "level 8")));
